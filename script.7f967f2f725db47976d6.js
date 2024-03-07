f8ef98157ee730d13122339ebc9dbf9ef2d4052c
BazBixxxy <133676922+BazBixxxy@users.noreply.github.com>
2024-03-07 12:32:29 +0300

    Initial commit

---
+++ script.7f967f2f725db47976d6.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!********************************!*\
+  !*** ./src/js-files/script.js ***!
+  \********************************/
+console.log('hi, am in the script file');
+/******/ })()
+;
+//# sourceMappingURL=script.7f967f2f725db47976d6.js.map
