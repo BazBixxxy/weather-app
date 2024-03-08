3362e5f1663d4b9502d6da188b73ca322937aad7
BazBixxxy <133676922+BazBixxxy@users.noreply.github.com>
2024-03-08 19:45:46 +0300

    completed the weather app, docs in the readme file

--- script.d56d4867d153c1cabe7b.js
+++ script.d56d4867d153c1cabe7b.js
@@ -3,7 +3,6 @@ var __webpack_exports__ = {};
 /*!********************************!*\
   !*** ./src/js-files/script.js ***!
   \********************************/
-console.log('hi, am in the script file');
+
 /******/ })()
 ;
-//# sourceMappingURL=script.7f967f2f725db47976d6.js.map
